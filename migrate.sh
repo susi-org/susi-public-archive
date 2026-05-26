22bfbd08607d6048a3d6786ecbe61a6017a7a1e5
Ján Plachý <jan.plachy@trojsten.sk>
2026-05-26 16:55:10 +0300

    Improve the migration script

--- migrate.sh
+++ migrate.sh
@@ -4,23 +4,23 @@ set -ueo pipefail
 
 if [[ ! -e 2020_21 ]]; then
     if [[ ! -e 01 ]]; then gh repo clone susi-org/01; fi
-    cp 01 2020_21
+    cp -r 01 2020_21
 fi
 if [[ ! -e 2021_22 ]]; then
     if [[ ! -e 02 ]]; then gh repo clone susi-org/02; fi
-    cp 02 2021_22
+    cp -r 02 2021_22
 fi
 if [[ ! -e 2022_23 ]]; then
     if [[ ! -e 03 ]]; then gh repo clone susi-org/03; fi
-    cp 03 2022_23
+    cp -r 03 2022_23
 fi
 if [[ ! -e 2023_24 ]]; then
     if [[ ! -e 04 ]]; then gh repo clone susi-org/04; fi
-    cp 04 2023_24
+    cp -r 04 2023_24
 fi
 if [[ ! -e 2024_25 ]]; then
     if [[ ! -e 05 ]]; then gh repo clone susi-org/05; fi
-    cp 05 2024_25
+    cp -r 05 2024_25
 fi
 
 mvc() {
@@ -31,7 +31,7 @@ mvc() {
 for year in *_*; do
     echo --- "$year"
     cd "$year"
-    rm -fr .git*
+    rm -fr .git .github
     rm -f meta.yaml README.md
     mvc 1 1-zima
     mvc 2 2-leto
@@ -53,6 +53,7 @@ for year in *_*; do
                     name=$(cat meta.yaml | head -n 1 | cut -d '"' -f 2)
                     author=$(cat meta.yaml | head -n 2 | tail -n 1 | cut -d '"' -f 2)
                     echo "$prob": "$name", "$author"
+                    if [[ -z "$author" ]]; then echo "! EMPTY AUTHOR LIST"; fi
                     mvc solution.md solution.md.tmp
                     echo Autori: "$author" > solution.md
                     echo "" >> solution.md

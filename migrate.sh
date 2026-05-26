22b87cbc08c6b3bd86a13df5909225bbc1f0a061
Ján Plachý <jan.plachy@trojsten.sk>
2026-05-26 16:49:14 +0300

    Add script for automatic migration and .gitignore

---
+++ migrate.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+if [[ ! -e 2020_21 ]]; then
+    gh repo clone susi-org/01;
+    cp 01 2020_21
+fi
+if [[ ! -e 2021_22 ]]; then
+    gh repo clone susi-org/02;
+    cp 02 2021_22
+fi
+if [[ ! -e 2022_23 ]]; then
+    gh repo clone susi-org/03;
+    cp 03 2022_23
+fi
+if [[ ! -e 2023_24 ]]; then
+    gh repo clone susi-org/04;
+    cp 04 2023_24
+fi
+if [[ ! -e 2024_25 ]]; then
+    gh repo clone susi-org/05;
+    cp 05 2024_25
+fi
+
+mvc() {
+    # conditional mv
+    if [[ -e "$1" ]]; then mv "$1" "$2"; fi
+}
+
+for year in *_*; do
+    echo --- "$year"
+    cd "$year"
+    rm -fr .git*
+    rm -f meta.yaml README.md
+    mvc 1 1-zima
+    mvc 2 2-leto
+    for part in *; do
+        echo -- "$part"
+        cd "$part"
+        rm -f meta.yaml
+        mvc 1 1-kolo
+        mvc 2 2-kolo
+        mvc 3 3-kolo
+        mvc outdoor objavne-kolo
+        for round in *; do
+            echo - "$round"
+            cd "$round"
+            rm -f meta.yaml
+            for prob in *; do
+                if [[ -f "$prob/meta.yaml" ]]; then
+                    cd "$prob"
+                    name=$(cat meta.yaml | head -n 1 | cut -d '"' -f 2)
+                    author=$(cat meta.yaml | head -n 2 | tail -n 1 | cut -d '"' -f 2)
+                    echo "$prob": "$name", "$author"
+                    mvc solution.md solution.md.tmp
+                    echo Autori: "$author" > solution.md
+                    echo "" >> solution.md
+                    cat solution.md.tmp >> solution.md
+                    rm -f solution.md.tmp meta.yaml
+                    cd ..
+                    mvc "$prob" "$prob-$(echo "$name" | tr ' ' '_')"
+                else
+                    echo "$prob": "$(head -n 1 $prob/solution.md)";
+                fi
+            done
+            cd ..
+        done
+        cd ..
+    done
+    cd ..
+done
